c5af53424dde2146f1b32206ffe983c489a750b2
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-07-19 01:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', (event) => {
+    const codes = document.querySelectorAll('.code');
+
+    codes.forEach((code, idx) => {
+        code.addEventListener('keydown', (e) => {
+            if (e.key >= 0 && e.key <= 9) {
+                codes[idx].value = ''; // Reset the current value
+                setTimeout(() => codes[idx + 1]?.focus(), 10); // Move to next input
+            } else if (e.key === 'Backspace') {
+                setTimeout(() => codes[idx - 1]?.focus(), 10); // Move to previous input
+            }
+        });
+    });
+});
